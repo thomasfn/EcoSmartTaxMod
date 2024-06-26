1436887fcf0c54a4b4f9bbc118845c045618f445
Thomas Smith <thomasfn@fortfn.co.uk>
2024-06-26 16:24:51 +0100

    Updated to Eco 0.10.3.0 playtest build 410

--- EcoSmartTaxMod/Logger.cs
+++ EcoSmartTaxMod/Logger.cs
@@ -1,9 +1,7 @@
-﻿using System;
-
-namespace Eco.Mods.SmartTax
+﻿namespace Eco.Mods.SmartTax
 {
     using Shared.Localization;
-    using Shared.Utils;
+    using Shared.Logging;
 
     public static class Logger
     {
